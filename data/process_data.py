ae73e909b3ffa2af46893794f2754518c7328b88
BurgerWu <64818741+BurgerWu@users.noreply.github.com>
2021-02-23 00:13:47 +0800

    Update Python Scripts
    Update Python Scripts

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,50 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
-
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, how="inner", on='id')
+    return df
 
 def clean_data(df):
-    pass
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(";", expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[0, :]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything
+    # up to the second to last character of each string with slicing
+    category_colnames = row.map(lambda x: x[:-2])
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].map(lambda x: x[-1])
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from `df`
+    df.drop(columns=["categories"], inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1, sort=False)
+
+    # drop duplicates
+    df = df[~(df.duplicated())]
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql("Disaster_Response_Table", engine, index=False)
 
 
 def main():
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,16 +1,72 @@
 import sys
+import pandas as pd
+import numpy as np
+import nltk
+import re
+from nltk.tokenize import word_tokenize
+from sqlalchemy import create_engine
+from sklearn.multioutput import MultiOutputClassifier
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
 
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 def load_data(database_filepath):
-    pass
+    # load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('Disaster_Response_Table', engine)
+ 
+    #Create X and Y 
+    X = df["message"].values
+    Y = df.iloc[:,4:].values
 
+    #Retrieve number of labels and label names
+    num_of_labels = Y.shape[1]
+    label_names = df.columns[4:]
+    return X, Y, label_names
 
 def tokenize(text):
-    pass
+    """
+    This tokenize function processes input text to generate useful word tokens
+    Input: text content
+    Output: processed word tokens    
+    """
+    #Define stop_words and create stemming and lemmatizing objects
+    stop_words = stopwords.words("english")
+    stemming = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
+    
+    #First use regular expression to get rid of punctuations
+    text = re.sub(r'[^a-zA-Z0-9]',' ',text.lower())
+    
+    #Then tokenize the text and eliminate stop words
+    words = word_tokenize(text)
+    words = [w for w in words if w not in stopwords.words("english")]
+    
+    #In the last step, process the word token list with lemmitizer and then stemmer
+    lemmed = [WordNetLemmatizer().lemmatize(w) for w in words]
+    stemmed = [PorterStemmer().stem(w) for w in lemmed]
+   
+    return stemmed
 
 
 def build_model():
-    pass
+    #Create pipeline object
+    Original_pipeline = Pipeline(
+    [("Vectorizer",CountVectorizer(tokenizer = tokenize)),\
+    ("TFIDF",TfidfTransformer()),\
+    ("Estimator",MultiOutputClassifier(RandomForestClassifier()))])
+
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
@@ -28,19 +84,19 @@ def main():
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
         
-        print('Building model...')
-        model = build_model()
+        #print('Building model...')
+        #model = build_model()
         
-        print('Training model...')
-        model.fit(X_train, Y_train)
+        #print('Training model...')
+        #model.fit(X_train, Y_train)
         
-        print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+       # print('Evaluating model...')
+       # evaluate_model(model, X_test, Y_test, category_names)
 
-        print('Saving model...\n    MODEL: {}'.format(model_filepath))
-        save_model(model, model_filepath)
+        #print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        #save_model(model, model_filepath)
 
-        print('Trained model saved!')
+        #print('Trained model saved!')
 
     else:
         print('Please provide the filepath of the disaster messages database '\
